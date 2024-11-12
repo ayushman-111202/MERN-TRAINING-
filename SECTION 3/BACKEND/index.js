fbe4daa29c9ae9442a33c7a235dd9d5fb0a316c1
ayushman-111202 <AYUSHMANSHUKLA.2021@outlook.com>
2024-11-12 20:34:02 +0530

    backend added

---
+++ SECTION 3/BACKEND/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+//creating an express app
+
+const app = express();
+
+const port = 5000;
+
+//starting the server
+
+app.listen(port, () => {
+    console.log('server started');
+})
