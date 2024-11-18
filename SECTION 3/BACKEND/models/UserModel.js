22c731b33e833bcd15b2edd0b63821c653fbffc0
ayushman-111202 <ayushmanshukla.2021@outlook.com>
2024-11-18 15:23:50 +0530

    db operations

--- SECTION 3/BACKEND/models/UserModel.js
+++ SECTION 3/BACKEND/models/UserModel.js
@@ -3,7 +3,8 @@ const { Schema, model } = require('../connection');
 const mySchema = new Schema({
     name: String,
     email: { type: String, unique : true },
-    password: { type: String, require: true },
+    password: { type: String, required: true },
+    city: { type: String, default: 'Unknown' },
     createdAt: { type: Date, default: Date.now }
 });
 
--- SECTION 3/BACKEND/routers/userRouter.js
+++ SECTION 3/BACKEND/routers/userRouter.js
@@ -1,30 +1,56 @@
 const express = require('express');
+const Model = require('../models/UserModel');
 
 const router = express.Router();
 
 //add
-router.post('/add',(req, res) => {
+router.post('/add', (req, res) => {
     console.log(req.body);
-    res.send('response from user add');
+    new Model(req.body).save()
+        .then((result) => {
+            res.status(200).json(result)
+        }).catch((err) => {
+            res.status(500).json(err);
+        });
 });
 
 //getall
-router.post('/getall',(req, res) => {
-    res.send('response from user getall');
+router.post('/getall', (req, res) => {
+    Model.find()
+        .then((result) => {
+            res.status(200).json(result)
+        }).catch((err) => {
+            res.status(500).json(err)
+        });
 });
 
+//denotes url parameter
+router.get('/getbycity/:city', (req, res) => {
+    Model.find({ city: req.params.city })
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            res.status(500).json(err);
+        });
+})
+
 //betbyid
-router.post('/getbyid',(req, res) => {
-    res.send('response from user getbyid');
+router.get('/getbyid/:id', (req, res) => {
+    Model.findById(req.params.id)
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            res.status(500).json(err);
+        });
 });
 
 // delete
-router.post('/delete',(req, res) => {
+router.post('/delete', (req, res) => {
     res.send('response from user delete');
 });
 
 //update
-router.post('/update',(req, res) => {
+router.post('/update', (req, res) => {
     res.send('response from user update');
 });
 
